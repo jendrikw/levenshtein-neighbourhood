e0944ce7341e313a7d574cc183d9663484d2a9a2
Jendrik <jendrik@deoxys>
2023-09-18 15:28:24 +0200

    newest update; seems slower (~116s)

--- src/main.rs
+++ src/main.rs
@@ -38,18 +38,18 @@ fn count_fixed_distance(s: &str, k: u32) -> usize {
     let mut valid = BTreeSet::new();
     while let Some((current, bit_count)) = queue.pop_front() {
         let next_dist = distances.get(&current).unwrap() + 1;
-        let mut next_bit_count = bit_count - 1;
-        for next_gen in [
-            Box::new((0..bit_count).map(|e| (current >> (e + 1) << e) + current % (1 << e))) as Box<dyn Iterator<Item=i32>>,
-            Box::new((0..bit_count).map(|e| current ^ (1 << e))),
-            Box::new((0..bit_count).map(|e| (current >> e << 1 << e) + current % (1 << e))),
-            Box::new((0..bit_count).map(|e| ((current >> e << 1 | 1) << e) + current % (1 << e))),
-        ] {
-            for next in next_gen {
-                // if distances.contains_key(&next) {
-                //     continue;
-                // }
-                // distances.insert(next, next_dist);
+        for e in 0..bit_count {
+            let left = current >> e;
+            let left_1 = left << 1;
+            let pow = 1 << e;
+            let right = current % pow;
+            let bp1 = bit_count + 1;
+            for (next, next_bit_count) in [
+                ((left >> 1 << e) | right, bit_count - 1),
+                (current ^ pow, bit_count),
+                ((left_1 << e) | right, bp1),
+                (((left_1 | 1) << e) | right, bp1)
+            ] {
                 match distances.entry(next) {
                     Entry::Vacant(ve) => {
                         ve.insert(next_dist);
@@ -62,7 +62,20 @@ fn count_fixed_distance(s: &str, k: u32) -> usize {
                     valid.insert(next);
                 }
             }
-            next_bit_count += 1;
+
+            for next in [pow << 2 | current, 3 << bit_count ^ current] {
+                match distances.entry(next) {
+                    Entry::Vacant(ve) => {
+                        ve.insert(next_dist);
+                    }
+                    Entry::Occupied(_) => continue,
+                }
+                if next_dist < k {
+                    queue.push_back((next, bit_count + 1));
+                } else {
+                    valid.insert(next);
+                }
+            }
         }
     }
     valid.len()
